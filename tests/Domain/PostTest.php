624d10f4db00614587a294d9f25e55a7a8bc72da
1@1.1 <1@1.1>
2019-11-06 17:54:31 +0000

    added some UnitTests

--- tests/Domain/PostTest.php
+++ tests/Domain/PostTest.php
@@ -10,11 +10,35 @@ use PHPUnit_Framework_TestCase;
  */
 class PostTest extends PHPUnit_Framework_TestCase
 {
+	private $postForTest;
+
+	public function setUp()
+	{
+	$this->postForTest = new Post (1,'hot');
+	$this->postForTest->setPostId(1);
+    $this->postForTest->setUserName('kolobok');
+    $this->postForTest->setPostDate(date('y-m-d'));
+	}
 	
 	public function testGetPostId()
 	{
-		$post = new Post();
-		$post->setPostId(1);
-		$this->assertSame (1, $post->getPostId(),"OOOOPS!");
+		$this->assertSame (1, $this->postForTest->getPostId(),"OOOOPS!");
+	}
+	
+	public function testGetPost()
+	{
+		$this->assertSame ('hot', $this->postForTest->getPost(),"OOOOPS!");
+	}	
+	public function testGetUserId()
+	{
+		$this->assertSame (1, $this->postForTest->getUserId(),"OOOOPS!");
+	}
+	public function testGetUserName()
+	{
+		$this->assertSame ('kolobok', $this->postForTest->getUserName(),"OOOOPS!");
+	}
+	public function testGetPostDate()
+	{
+		$this->assertSame (date('y-m-d'), $this->postForTest->getPostDate(),"OOOOPS!");
 	}
 }
